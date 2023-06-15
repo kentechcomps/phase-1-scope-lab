a34470e0f71dd107bc2799d993dacd89c2c899e0
user <mutukukennedy34@gmail.com>
2023-06-15 21:42:04 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob'
+var bestCustomer = 'not bob'
+const leastFavoriteCustomer ='jake'
+
+function upperCaseCustomerName(){
+    console.log(customerName.toUpperCase);
+}
+function setBestCustomer(){
+  var bestCustomer ='poorbob'
+  console.log(bestCustomer);  
+}
+function overwriteBestCustomer(){
+    var bestCustomer = 'maybe bob'
+    console.log(bestCustomer);
+}
+function changeLeastFavoriteCustomer(){
+    var leastFavoriteCustomer = 'joke'
+    console.log(leastFavoriteCustomer);
+}
+
